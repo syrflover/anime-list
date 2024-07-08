ff953045d1954b22f8da45eed1d8de945289430a
syr <user@syrflover.co>
2024-07-08 13:39:41 +0900

    deploy to gh-pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,17 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `anime`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [],
-}
+    // siteMetadata: {
+    //   title: `syr's anime list`,
+    //   siteUrl: `https://www.yourdomain.tld`,
+    // },
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [],
 
-export default config
+    pathPrefix: "/anime-list",
+};
+
+export default config;
